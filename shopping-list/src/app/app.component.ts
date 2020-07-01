667bc12d924fe65a769a6c1e728ea227e694d081
Wigwa <lucywigwa7@gmail.com>
2020-07-01 13:09:06 +0300

    my first commit

---
+++ shopping-list/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ title = "shopping list";
+ shopping:string[];
+
+ constructor(){
+   this.shopping = ['fruits', 'cloths','snaks']
+ }
+}
