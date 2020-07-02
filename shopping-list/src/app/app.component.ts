20ee24c89cd7017a51c37eb03c422e94b01fc799
Wigwa <lucywigwa7@gmail.com>
2020-07-02 04:14:01 +0300

    add div class intro

--- shopping-list/src/app/app.component.ts
+++ shopping-list/src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  title = "shopping list";
- shopping:string[];
-
- constructor(){
-   this.shopping = ['fruits', 'cloths','snaks']
- }
 }
