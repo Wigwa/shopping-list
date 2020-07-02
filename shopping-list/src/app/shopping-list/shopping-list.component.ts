104198ee5340fd16a1038d087482602027aee109
Wigwa <lucywigwa7@gmail.com>
2020-07-02 10:13:08 +0300

    modify

--- shopping-list/src/app/shopping-list/shopping-list.component.ts
+++ shopping-list/src/app/shopping-list/shopping-list.component.ts
@@ -7,41 +7,41 @@ import { Shoppinglist } from '../shoppinglist'
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-shoppinglist:Shoppinglist[] = [
-  new Shoppinglist ('Fruits(Apple,Mango)', '1Okg', 'Ksh.1500', new Date(2020,7,2)),
+  ngOnInit() {
+    
+  }
+  shoppinglist: Shoppinglist[] = [
+    new Shoppinglist('Fruits(Apple,Mango)', '1Okg', 'Ksh.1500', new Date(2020, 7, 2)),
 
-  new Shoppinglist ('Maize Flour(Pembe)', '2Bundles', 'Ksh.2200', new Date(2020,7,2)),
+    new Shoppinglist('Maize Flour(Pembe)', '2Bundles', 'Ksh.2200', new Date(2020, 7, 2)),
 
-  new Shoppinglist ('Vegetables(SukumaWiki,Tomatoes,Onions)','2Kg each', 'Ksh.500', new Date(2020,7,2)),
+    new Shoppinglist('Vegetables(SukumaWiki,Tomatoes,Onions)', '2Kg each', 'Ksh.500', new Date(2020, 7, 2)),
 
-  new Shoppinglist ('Sugar(Naivas Sugar)', '10Kg', 'Ksh.1000', new Date(2020,7.2)),
-  new Shoppinglist ('Wheat Flour(Pembe)', '1Bundle', 'Ksh.1200', new Date(2020,7,2))
-];
+    new Shoppinglist('Sugar(Naivas Sugar)', '10Kg', 'Ksh.1000', new Date(2020, 7.2)),
+    new Shoppinglist('Wheat Flour(Pembe)', '1Bundle', 'Ksh.1200', new Date(2020, 7, 2))
+  ];
 
-seeDetail(index) {
-  this.shoppinglist[index].viewDetails = !this.shoppinglist
-  [index].viewDetails;
-  purgeShoppinglist( , index);{
-    if (isDone) {
-      let toDelete = confirm('Are you sure you want to delete this shoppinglist');
+  seeDetail(index) {
+    this.shoppinglist[index].viewDetails = !this.shoppinglist
+    [index].viewDetails;
+  }
+  purgeShoppingList(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete this Qoute`);
       if (toDelete) {
         this.shoppinglist.splice(index, 1);
-
       }
-
-    }
     }
   }
-  addNewShoppinglist(luc) {
-    luc.elapse = new Date(luc.elapse)
-    this.shoppinglist.push(luc);
+addNewShoppinglist(luc) {
+  luc.elapse = new Date(luc.elapse)
+  this.shoppinglist.push(luc);
 
-  }
-
-  
+}
 
 
-  ngOnInit(){
-  }
+
+
+
 
 }
