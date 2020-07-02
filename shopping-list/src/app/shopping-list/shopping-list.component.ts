615caf98b6df72deada1aad7ac5e821579c5ac95
Wigwa <lucywigwa7@gmail.com>
2020-07-02 04:54:58 +0300

    add shopping item

--- shopping-list/src/app/shopping-list/shopping-list.component.ts
+++ shopping-list/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ShoppingList } from '..'
+import { Shoppinglist } from '../shoppinglist'
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +7,17 @@ import { ShoppingList } from '..'
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+shoppinglist:Shoppinglist[] = [
+  new Shoppinglist ('Fruits(Apple,Mango)', '1Okg', 'Ksh.1500', new Date(2020,7,2)),
+
+  new Shoppinglist ('Maize Flour(Pembe)', '2Bundles', 'Ksh.2200', new Date(2020,7,2)),
+
+  new Shoppinglist ('Vegetables(SukumaWiki,Tomatoes,Onions)','2Kg each', 'Ksh.500', new Date(2020,7,2)),
+
+  new Shoppinglist ('Sugar(Naivas Sugar)', '10Kg', 'Ksh.1000', new Date(2020,7.2)),
+  new Shoppinglist ('Wheat Flour(Pembe)', '1Bundle', 'Ksh.1200', new Date(2020,7,2))
+];
+
 
   
 
---
+++ shopping-list/src/app/shoppinglist.ts
@@ -0,0 +1,12 @@
+export class Shoppinglist {
+    viewDetails: boolean;
+    constructor (
+        public item: string, 
+        public quantity: string,
+        public aproximateprice: string,
+        public elapse: Date
+    ) {
+        this.viewDetails = false;
+    }
+    }
+
